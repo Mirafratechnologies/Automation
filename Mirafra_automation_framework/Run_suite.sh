f97c984062142040900992e6a0a1fad073160829
Sharma <sunilpatlamane@mirafra.com>
2016-09-14 12:28:27 +0530

    Added python path

--- Mirafra_automation_framework/Run_suite.sh
+++ Mirafra_automation_framework/Run_suite.sh
@@ -2,14 +2,22 @@
 
 if [ -z "$1" ]
   then
-    echo "Usage:" $0 " Testcases_file output_file\n"
+    echo "Usage:" $0 " Testcases_file output_file testcase_result_file"
     exit
 fi
 
 if [ -z "$2" ]
   then
-    echo "Usage:" $0 " Testcases_file output_file\n"
+    echo "Usage:" $0 " Testcases_file output_file testcase_result_file"
     exit
 fi
 
-./testsuite.py $1 |& tee $2
+if [ -z "$3" ]
+  then
+    echo "Usage:" $0 " Testcases_file output_file testcase_result_file"
+    exit
+fi
+
+export PYTHONPATH=$PYTHONPATH:/home/sunilpatlamane/Mirafra_automation_framework
+
+./testsuite.py $1 $3 |& tee $2
